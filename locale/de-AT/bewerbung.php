901ee909ba56cee7c409ffb2de32fb446c2633d5
Gerry Raab <raab@technikum-wien.at>
2016-09-10 00:00:54 +0200

    texte onlinebewerbung

---
+++ locale/de-AT/bewerbung.php
@@ -0,0 +1,5 @@
+<?php
+
+$this->phrasen['bewerbung/sieHabenNochKeinenZugangscode']='Sie haben noch keinen Zugangscode oder Account an SFU Wien?';
+$this->phrasen['bewerbung/studierenOderArbeitenSieBereits']='Studieren oder arbeiten Sie bereits an der SFU Wien?';
+?>
