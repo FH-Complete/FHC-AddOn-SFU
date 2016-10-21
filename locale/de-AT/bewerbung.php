3a78a2fc8367876c8f88ebce90c840ba1ac40751
Gerald Raab <raab@technikum-wien.at>
2016-10-21 17:42:58 +0200

    typos

--- locale/de-AT/bewerbung.php
+++ locale/de-AT/bewerbung.php
@@ -1,5 +1,6 @@
 <?php
 
-$this->phrasen['bewerbung/sieHabenNochKeinenZugangscode']='Sie haben noch keinen Zugangscode oder Account an SFU Wien?';
+$this->phrasen['bewerbung/sieHabenNochKeinenZugangscode']='Sie haben noch keinen Zugangscode oder Account an der SFU Wien?';
 $this->phrasen['bewerbung/studierenOderArbeitenSieBereits']='Studieren oder arbeiten Sie bereits an der SFU Wien?';
+$this->phrasen['bewerbung/hierUnverbindlichAnmelden']='Hier anmelden';
 ?>
